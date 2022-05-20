0ba3c4b3338149a338f14972b5f1c1039680a669
Landry <landryjere689@gmail.com>
2022-05-20 16:45:11 -0300

    styles added + logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCircleQuestion, faSquarePollVertical} from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import Logo from './chardleLogo.png'
 
 function NavBar() {
   return (
@@ -10,7 +11,8 @@ function NavBar() {
             <FontAwesomeIcon icon={faCircleQuestion}/>
         </Link>
         <Link to='/' className='how-to-play-link'>
-            <h1 className='navbar-title'>CHARDLE</h1>
+            <img src={Logo} className='chardleLogo'/>
+            {/* <h1 className='navbar-title'>CHARDLE</h1> */}
         </Link>
         <Link to='/statistics' className='how-to-play-link'>
             <FontAwesomeIcon icon={faSquarePollVertical}/>
