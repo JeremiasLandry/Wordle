aee9d331dd9e86caed1fa429d9c5e9e626350062
Landry <landryjere689@gmail.com>
2022-05-20 14:52:36 -0300

    howToPlay examples now has other words

--- src/components/HowToPlayPage/HowToPlayPage.js
+++ src/components/HowToPlayPage/HowToPlayPage.js
@@ -9,9 +9,9 @@ function HowToPlayPage() {
     <div className='how-to-play'>
         <h1>HOW TO PLAY</h1>
         <div className="text-container">
-            <span>Guess the <b>WORDLE</b> in six tries</span>
+            <span>Guess the <b>CHARDLE</b> in six tries</span>
             <br/>
-            <span>Each guess must be a valid five-letter word. Hit the enter button to submit</span>
+            <span>Each guess must be a valid five-letter word conforming a fictional character's name. Hit the enter button to submit</span>
             <br/>
             <span>After each guess, the color of the tiles will change to show how close your guess was to the word.</span>
         </div>
@@ -22,42 +22,42 @@ function HowToPlayPage() {
             </p>
             <div className='example'>
                 <div className='row'>
-                    <GameTile letter='w' evaluation='correct'/>
-                    <GameTile letter='e'/>
+                    <GameTile letter='s' evaluation='correct'/>
+                    <GameTile letter='t'/>
                     <GameTile letter='a'/>
                     <GameTile letter='r'/>
-                    <GameTile letter='y'/>
+                    <GameTile letter='k'/>
                 </div>
                 <p>
-                The letter <strong>W</strong> is in the word and in the correct spot.
+                The letter <strong>S</strong> is in the word and in the correct spot.
                 </p>
             </div>
             <div className='example'>
                 <div className='row'>
-                    <GameTile letter='p'/>
-                    <GameTile letter='i' evaluation='almost'/>
-                    <GameTile letter='l'/>
-                    <GameTile letter='l'/>
-                    <GameTile letter='s'/>
+                    <GameTile letter='f'/>
+                    <GameTile letter='r' evaluation='almost'/>
+                    <GameTile letter='o'/>
+                    <GameTile letter='d'/>
+                    <GameTile letter='o'/>
                 </div>
                 <p>
-                The letter <strong>I</strong> is in the word but in the wrong spot.
+                The letter <strong>R</strong> is in the word but in the wrong spot.
                 </p>
             </div>
             <div className='example'>
                 <div className='row'>
-                    <GameTile letter='v'/>
                     <GameTile letter='a'/>
-                    <GameTile letter='g'/>
-                    <GameTile letter='u' evaluation='notInTheWord'/>
+                    <GameTile letter='n'/>
+                    <GameTile letter='n'/>
+                    <GameTile letter='i' evaluation='notInTheWord'/>
                     <GameTile letter='e'/>
                 </div>
                 <p>
-                The letter <strong>U</strong> is not in the word in any spot.
+                The letter <strong>I</strong> is not in the word in any spot.
                 </p>
             </div>
         </div>
-        <p style={{marginBottom:25+'px'}}><strong>A new WORDLE will be available each day!</strong></p>
+        <p style={{marginBottom:25+'px'}}><strong>A new CHARDLE will be available each day!</strong></p>
         <Link to='/' className='getBack-btn'>
             <FontAwesomeIcon icon={faArrowLeft}/>
             <span style={{marginLeft:8+'px'}}>
